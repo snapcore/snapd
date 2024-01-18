cf444f43171cfec7aa37f9ecd206eda269862f3a
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2024-01-18 15:29:16 +0100

    More changes requested

--- overlord/snapstate/snapstate.go
+++ overlord/snapstate/snapstate.go
@@ -86,6 +86,8 @@ const (
 	snapdDesktopIntegrationSnapID = "IrwRHakqtzhFRHJOOPxKVPU0Kk7Erhcu"
 )
 
+const defaultMaxInhibitionDays = 14
+
 var ErrNothingToDo = errors.New("nothing to do")
 
 var osutilCheckFreeSpace = osutil.CheckFreeSpace
@@ -314,9 +316,8 @@ func refreshRetain(st *state.State) int {
 	return retain
 }
 
-// maxInhibitionTime returns the value of the maximum inhibition time, in seconds
+// maxInhibitionTime returns the value of the maximum inhibition time
 func maxInhibitionTime(st *state.State) time.Duration {
-
 	var maxInhibitionDays int
 	err := config.NewTransaction(st).Get("core", "refresh.max-inhibition-days", &maxInhibitionDays)
 
@@ -326,7 +327,7 @@ func maxInhibitionTime(st *state.State) time.Duration {
 
 	// not set, use default value
 	if maxInhibitionDays == 0 {
-		maxInhibitionDays = 14
+		maxInhibitionDays = defaultMaxInhibitionDays
 	}
 
 	// deduct 1s so it doesn't look confusing initially when two notifications
