c1ab559802ec941ed413f92ea6a41a43a85402a6
Andrew Phelps <andrew.phelps@canonical.com>
2024-03-14 12:20:03 -0400

    o/snapstate: create setup-profiles task when installing a component

--- overlord/snapstate/component_install_test.go
+++ overlord/snapstate/component_install_test.go
@@ -60,6 +60,9 @@ func expectedComponentInstallTasks(opts int) []string {
 	if opts&compOptIsActive != 0 {
 		startTasks = append(startTasks, "unlink-current-component")
 	}
+
+	startTasks = append(startTasks, "setup-profiles")
+
 	// link-component is always present
 	startTasks = append(startTasks, "link-component")
 
